2a81572064c7cd62486c870300fb468fce95019a
张伟 <kansee@gmail.com>
2019-11-13 18:52:57 +0800

    add

---
+++ bin/index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+const colors = require('colors');
+const greet = require("../lib/greet");
+
+// print random greeting
+console.log(
+  // wraps text with rainbow color formatting
+  colors.rainbow(
+    // returns the random greeting text
+    "Hello World"
+  )
+);
