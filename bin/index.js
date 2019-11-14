c2480d6650935199af9b42e5b323c06d6a640420
张伟 <kansee@gmail.com>
2019-11-14 12:41:00 +0800

    add

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
 const colors = require('colors');
-const archy = require('archy')
+const archy = require('archy');
+const chalk = require('chalk');
+const figlet = require('figlet');
 const greet = require("../lib/greet");
 const architecture = require('../architecture');
 const menu = archy(architecture);
 
 console.log(
-  colors.rainbow("恭喜您使用云帧数浪工具箱")
+  chalk.green(
+    figlet.textSync('Cloud Wave', { horizontalLayout: 'full' })
+  )
 );
 
 console.log(menu)
--- test.js
+++
@@ -1,3 +0,0 @@
-const SayHello = require("./index");
-
-SayHello();
