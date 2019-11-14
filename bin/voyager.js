604e55c92fd603858d405cfd46a0336542f36377
张伟 <kansee@gmail.com>
2019-11-14 16:33:05 +0800

    add code

--- bin/voyager.js
+++ bin/voyager.js
@@ -1,12 +1,35 @@
 #!/usr/bin/env node
+const path = require("path");
+const program = require('commander');
+const fs = require('fs-extra');
+const vfs = require('vinyl-fs');
+const archy = require('archy');
+const architecture = require('../architecture');
+const menu = archy(architecture);
 
-let program = require('commander');
+
 program
   .version('0.0.1')
-  .option('-i, --init', 'init a HTML template')
+  .option('-i, --init [name]', 'init a HTML template')
 
 program.parse(process.argv);
 
 if (program.init) {
-  console.log("init a HTML template")
+  let projectDirName = program.init;
+  if (typeof projectDirName === "string") {
+    var projectPath = path.resolve(projectDirName);
+    console.log(`Initialing a next.js project in ${projectPath}`);
+    fs.ensureDirSync(program.init);
+    var templatePath = path.join(__dirname, '../template');
+    vfs.src(['*', '!node_modules/*'], { cwd: templatePath, dot: true })
+      .pipe(vfs.dest(projectDirName))
+      .on('end', function () {
+        console.log(menu)
+        console.log('Installing packages...')
+      })
+      .resume();
+
+  } else {
+    console.log("Lacking project name, Please input your project name after 'voyager --init'")
+  }
 }
