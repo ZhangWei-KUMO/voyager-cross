8a46aadb3a2823346863e29f2b452463deebfeba
张伟 <kansee@gmail.com>
2019-11-14 19:36:11 +0800

    add

---
+++ template/pages/blog.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Blog extends Component {
+  render() {
+    return (
+      <div>
+        博客
+      </div>
+    )
+  }
+}
+
+export default Blog;
---
+++ template/pages/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        欢迎进入我们的世界
+      </div>
+    )
+  }
+}
+
+export default HomePage;
