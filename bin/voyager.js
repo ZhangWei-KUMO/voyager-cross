dcbb6d6b8958a453c61275a4a31feaa7b2e304a5
张伟 <kansee@gmail.com>
2019-11-14 15:13:27 +0800

    add

--- bin/voyager.js
+++ bin/voyager.js
@@ -1,14 +1,12 @@
 #!/usr/bin/env node
+
 let program = require('commander');
 program
   .version('0.0.1')
-  .option('-d, --debug', 'output extra debugging')
-  .option('-s, --small', 'small pizza size')
-  .option('-p, --pizza-type <type>', 'flavour of pizza');
+  .option('-i, --init', 'init a HTML template')
 
 program.parse(process.argv);
 
-if (program.debug) console.log(program.opts());
-console.log('pizza details:');
-if (program.small) console.log('- small pizza size');
-if (program.pizzaType) console.log(`- ${program.pizzaType}`);
+if (program.init) {
+  console.log("init a HTML template")
+}
